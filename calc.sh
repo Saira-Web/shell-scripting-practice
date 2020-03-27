740c67efa72bb206f288dadc7db4b08c59424ef8
Saira Rehman <saira@Sairas-MacBook-Pro.local>
2020-03-27 15:33:41 -0400

    Add addition substract and exit option  to selct

--- calc.sh
+++ calc.sh
@@ -5,8 +5,11 @@ echo "Hello, Welcome to our interactive calculator program."
 # Then, display a menu that outlines the possible operations:
 echo " Please select one of the following arithmetic operators."
   # Add
+  echo "Addition"
   # Subtract
+  echo "Substraction"
   # Exit
+  echo "Exit Calculator"
 # Then, capture the user selection.
 # If the selection matches a supported operation, execute the operation.
 # If the selection does not match a supported operation, display an error message.
