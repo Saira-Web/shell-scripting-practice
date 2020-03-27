a0670f28752ef2eb667ae7ffc4ccf0e8cc22edef
Saira Rehman <saira@Sairas-MacBook-Pro.local>
2020-03-27 16:14:57 -0400

    Comments adjusted for better understaning of code

--- calc.sh
+++ calc.sh
@@ -13,8 +13,9 @@ echo " Please select one of the following arithmetic operators."
 # Then, capture the user selection.
 read selection
 
-# If the selection matches a supported operation, execute the operation.
+
 case $selection in
+# If the selection matches a supported operation, execute the operation.
 "Addition")
 echo "You have selected 'Addition'"
 ;;
@@ -24,12 +25,13 @@ echo "You have selected 'Substraction'"
 "Exit Calculator")
 echo "You have selected 'Exit Calculator'"
 ;;
+# If the selection does not match a supported operation, display an error message.
 *)
 echo "Sorry, this operation is not supported: $selection"
 echo "Please try again!"
 ;;
 esac
-# If the selection does not match a supported operation, display an error message.
+
 
 esac
 # When the operation is complete, redisplay the menu.
